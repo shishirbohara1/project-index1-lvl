337c794334b6bbb08b2f6b51c7c6fc43fda2055c
Your Name <you@example.comcls>
2022-09-09 23:31:19 +0545

    commit 1

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,57 @@
+<style>
+  footer {
+  position: relative;
+  width: 100%;
+  height: 400px;
+  background: #101010;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #fff;
+}
+.contact-info {
+  padding: 0 50px;
+}
+.social {
+  display: flex;
+}
+.social a {
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgb(224, 159, 39);
+  border-radius: 50%;
+  margin: 22px 10px;
+  color: white;
+  text-decoration: none;
+  font-size: 20px;
+}
+.social a:hover {
+  transform: scale(1.3);
+  transition: 0.3s;
+  cursor: pointer;
+}
+.end {
+  position: absolute;
+  color: rgb(224, 159, 39);
+  bottom: 35px;
+  font-size: 14px;
+}
+</style>
+<footer id="contact">
+	<h1 class="headings">Contact Info</h1>
+	<p class="contact-info"> 
+    +977-0000000000 <br>
+    infoo@social.com.np <br>
+    Shantinagar,Kathmandu</p>
+	<div class="social">
+		<a href="https://www.fb.com"><i class="fab fa-facebook-f"></i></a>
+		<a href="#"><i class="fab fa-instagram"></i></a>
+		<a href="#"><i class="fab fa-dribbble"></i></a>
+	</div>
+	<p class="end">CopyRight By Social</p>
+</footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" width="75" height="75">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#about">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#team">Our Team </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#contact">Contact Us </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
